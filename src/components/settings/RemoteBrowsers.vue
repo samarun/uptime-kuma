aefac1ec61934b0f335c7196a1503bc5bd248207
Arun Samuel <samspostbox@gmail.com>
2024-04-01 18:20:03 +0530

    Update RemoteBrowsers.vue

--- src/components/settings/RemoteBrowsers.vue
+++ src/components/settings/RemoteBrowsers.vue
@@ -15,12 +15,7 @@
             <button class="btn btn-primary me-2" type="button" @click="$refs.remoteBrowserDialog.show()">
                 <font-awesome-icon icon="plus" /> {{ $t("Add Remote Browser") }}
             </button>
-        </div>
-
-        <div class="my-4 pt-4">
-            <h5 class="my-4 settings-subheading">{{ $t("What is a Remote Browser?") }}</h5>
-            <p>{{ $t("remoteBrowsersDescription") }} <a href="https://hub.docker.com/r/browserless/chrome">{{ $t("self-hosted container") }}</a></p>
-        </div>
+        </div>       
 
         <RemoteBrowserDialog ref="remoteBrowserDialog" />
     </div>
